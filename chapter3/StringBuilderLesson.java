b9b282b3cd80a3be0d0b37b0a1aef1252ae6ee07
marcoyimyaem <marcoyimyaem@gmail.com>
2024-06-08 14:50:40 +0800

    before shutdown

--- chapter3/StringBuilderLesson.java
+++ chapter3/StringBuilderLesson.java
@@ -3,6 +3,7 @@ class StringBuilderLesson{
     public static void main(String[] args) {
         StringBuilder a = new StringBuilder("abc");
         StringBuilder b = new StringBuilder("abc");
+        StringBuilder c =a;
         int x;
         
     }
