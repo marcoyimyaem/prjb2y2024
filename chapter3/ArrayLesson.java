2c58333b1e2aaed72d86ba3365828d6bc597d711
marcoyimyaem <marcoyimyaem@gmail.com>
2024-06-08 15:11:08 +0800

    end of day 12

--- chapter3/ArrayLesson.java
+++ chapter3/ArrayLesson.java
@@ -7,7 +7,7 @@ public class ArrayLesson {
     public static void main(String[] args) {
         int[] numbers = new  int[3];
         System.out.println(numbers[2]);
-        String[] numbStrings = {"10","9","100", };
+        String[] numbStrings = {"10","9","100", " "};
         Arrays.sort(numbStrings);
         for (String numbstring : numbStrings) {
             System.out.println(numbstring);
